6b99e734e21b17cc6b64239cb1070f40936ab963
Random90 <gczernatowicz1@gmail.com>
2021-10-20 20:10:32 +0200

    setting time from sntp

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,73 @@
+
 #include "Arduino.h"
-int ledState = LOW;
+#include <ESP8266WiFi.h>
+#include <time.h>
+#include <sys/time.h>
+#include "time.h"
+#include <TZ.h>
+
+#define MYTZ TZ_Europe_Warsaw
+
+bool ledState = false;
 unsigned long previousMillis = 0;  
 
-const long interval = 1000;      
+static timeval tv;
+static time_t now;
+
+void vShowTime() {
+  gettimeofday(&tv, nullptr);
+  now = time(nullptr);
+  Serial.printf("The current timestamp: %ju\n", (uintmax_t)now);
+  // human readable
+  Serial.println(ctime(&now));
+}
+
+void vBlinkLed() {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= 1000) {
+    previousMillis = currentMillis;
+
+    ledState = !ledState;
+    digitalWrite(LED_BUILTIN, !ledState);
+    digitalWrite(D3, ledState);
+    vShowTime();
+  }
+}
+
+void vSetupWifi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin("xxx", "xxxx");
+  // Wait for connection
+  // TODO non-blocking plz
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected, ");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void vSetupTime() {
+  // TODO dynamic timezone
+  configTime(MYTZ, "pool.ntp.org");
+  Serial.printf("timezone:  %s\n", getenv("TZ") ? : "(none)");
+  vShowTime();
+}
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(D3, OUTPUT);
+
+  Serial.begin(74880);
+  Serial.println("Sunset WiFi Switch Init");
+
+  vSetupWifi();
+  vSetupTime();
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-
-    if (ledState == LOW) {
-      ledState = HIGH;
-    } else {
-      ledState = LOW;
-    }
-
-    digitalWrite(LED_BUILTIN, ledState);
-  }
+  vBlinkLed();
 }
